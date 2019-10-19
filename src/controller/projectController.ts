830b97d1e579669e14c60b5900a67025f9b0ba2f
past13 <laurynas.janilionis@gmail.com>
2019-10-20 00:51:32 +0200

    add controller

--- src/controller/projectController.ts
+++ src/controller/projectController.ts
@@ -1,4 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+
+const fs = require('fs');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
 
 export default class ProjectController {
+    public async uploadXmlFile (req: Request, res: Response) {
+        try {
+            let xml = __dirname + '/data.xml';
+            fs.readFile(xml, "utf-8", function (error: any, text:any) {
+                if (error) {
 
+                } else {
+                    parser.parseString(text, function (err:any, result:any) {
+                        var transaction = result['Transactions']['Transaction'];
+                    });
+                }
+            });
+
+            return res.json({ message: "valid xml" })
+
+        } catch (err) {
+            res.status(401).json(err);
+        }
+    }
 }
