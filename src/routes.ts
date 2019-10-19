70569d4456d108e0b81a323f928b23ae2e482acc
past13 <laurynas.janilionis@gmail.com>
2019-10-20 00:48:09 +0200

    add app mongoose express corse and routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
+import { Application } from 'express';
+import ProjectController from './controller/projectController';
 
 export class Routes {
+    public projectController: ProjectController = new ProjectController();
 
-  
+    public routes(app: Application): void {
+
+        // app.get('/uploadXmlFile', this.projectController.uploadXmlFile);
+    }
 }
